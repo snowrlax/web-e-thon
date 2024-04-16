2594f194ad9e682ce29ec2d50680e2be51208767
Rutuja anil kothawade <rutuja04@Rutujas-MacBook-Air.local>
2024-04-16 12:43:05 +0530

    changed nav img

--- src/components/MentorList.jsx
+++ src/components/MentorList.jsx
@@ -1,20 +1,20 @@
 export const List = () => {
     return (
         <>
-        <li className="py-3 sm:py-4">
-                <div class="flex items-center">
-                    <div class="flex-shrink-0">
-                        <img class="w-8 h-8 rounded-full" src="/docs/images/people/profile-picture-1.jpg" alt="Neil image" />
+        <li classNameName="py-3 sm:py-4">
+                <div className="flex items-center">
+                    <div className="flex-shrink-0">
+                        <img className="w-8 h-8 rounded-full" src="/docs/images/people/profile-picture-1.jpg" alt="Neil image" />
                     </div>
-                    <div class="flex-1 min-w-0 ms-4">
-                        <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
+                    <div className="flex-1 min-w-0 ms-4">
+                        <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
                             Neil Sims
                         </p>
-                        <p class="text-sm text-gray-500 truncate dark:text-gray-400">
+                        <p className="text-sm text-gray-500 truncate dark:text-gray-400">
                             email@windster.com
                         </p>
                     </div>
-                    <div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+                    <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
                         3 Yrs
                     </div>
                 </div>
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -3,8 +3,8 @@ import { List } from "../components/MentorList"
 
 export const Dashboard = () => {
     return (
-        <div className="mt-20 h-screen flex gap-2 flex-row justify-center">
-            <div className="w-[70%] flex flex-col gap-2">
+        <div classNameName="mt-20 h-screen flex gap-2 flex-row justify-center">
+            <div classNameName="w-[70%] flex flex-col gap-2">
                 
 
 <MentorCard />
@@ -12,18 +12,18 @@ export const Dashboard = () => {
 <MentorCard />
 
             </div>
-            <div className="">
+            <div classNameName="">
                 
 
-<div class="w-full max-w-md p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
-    <div class="flex items-center justify-between mb-4">
-        <h5 class="text-xl font-bold leading-none text-gray-900 dark:text-white">Industry Mentors</h5>
-        <a href="#" class="text-sm font-medium text-blue-600 hover:underline dark:text-blue-500">
+<div className="w-full max-w-md p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+    <div className="flex items-center justify-between mb-4">
+        <h5 className="text-xl font-bold leading-none text-gray-900 dark:text-white">Industry Mentors</h5>
+        <a href="#" className="text-sm font-medium text-blue-600 hover:underline dark:text-blue-500">
             View all
         </a>
    </div>
-   <div class="flow-root">
-        <ul role="list" class="divide-y divide-gray-200 dark:divide-gray-700">
+   <div className="flow-root">
+        <ul role="list" className="divide-y divide-gray-200 dark:divide-gray-700">
             <List />
             <List />
             <List />
