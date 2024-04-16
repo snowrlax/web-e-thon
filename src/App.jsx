6f5dadd0cfe66f95865bc0ef66a13862822387cd
Rutuja anil kothawade <rutuja04@Rutujas-Air.mshome.net>
2024-04-16 11:27:29 +0530

    add footer hero nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 
 import './App.css'
+import { Footer } from './components/Footer'
+import { Hero } from './components/Hero'
+import { Navbar } from './components/Navbar'
 
 function App() {
 
   return (
     <>
     <div className="">
-      Hello there!  
+      <Navbar />
+      <Hero />  
     </div>
+    <Footer />
       </>
   )
 }
